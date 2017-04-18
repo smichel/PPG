1dbd192ba548bf312e5c3c6292a7b4e0d2f338d7
Theresa Lang <lang@wr.informatik.uni-hamburg.de>
2017-04-18 10:51:12 +0200

    added comments.

--- 1b_GameOfLife/game_of_life.f90
+++ 1b_GameOfLife/game_of_life.f90
@@ -1,20 +1,21 @@
 program game_of_life
-use mod_lifecycle
-use mod_initializeField
-use mo_glider
-logical,allocatable :: playground(:,:)
-integer :: outputUnit = 6
-!playground(4,4) = .TRUE.
-!playground(4,5) = .TRUE.
-!playground(4,6) = .TRUE.
-open(outputUnit, encoding='UTF-8') ! change character set to UTF-8 for stdout,
-! printTwoDLogical needs that.
 
-call createField(30,20,playground)
-call createFigures(playground)
-call printTwoDLogical(6, playground)
-do i = 1,100
-	call developLife(playground)
-	call printTwoDLogical(outputUnit, playground(1:30,1:20))
-enddo
+	use mod_lifecycle
+	use mod_initializeField
+	use mo_glider
+
+	logical,allocatable :: playground(:,:)	! Spielfeld
+	integer :: outputUnit = 6
+
+	open(outputUnit, encoding='UTF-8') ! change character set to UTF-8 for stdout,
+	! wird fuer printTwoDLogical gebraucht
+
+	call createField(30,20,playground)
+	call createFigures(playground)
+	call printTwoDLogical(6, playground)
+
+	do i = 1,10
+		call developLife(playground)
+		call printTwoDLogical(outputUnit, playground(1:30,1:20))
+	enddo
 end program game_of_life
--- 1b_GameOfLife/mod_lifecycle.f90
+++ 1b_GameOfLife/mod_lifecycle.f90
@@ -1,12 +1,13 @@
 module mod_lifecycle
 	implicit none
-contains
+	contains
 	
 	subroutine countNeighbors(playground,neighbors)
-		logical, intent(in) :: playground(0:31,0:21)
-		integer, intent(out) :: neighbors(30,20)
-		integer :: counter = 0
-		integer :: i,j
+		logical, intent(in) :: playground(0:31,0:21)	! Spielfeld
+		integer, intent(out) :: neighbors(30,20)	! Feld mit Anzahl an Nachbarn
+		integer :: counter = 0	! Variable zum zahlen der Nachbarn einer Zelle
+		integer :: i,j	! Zaehlindices
+		! zaehle Nachbarn fuer jede Zelle auf dem Spielfeld 
 		do i = 1,30
 			do j = 1,20
 				counter = 0
@@ -18,20 +19,26 @@ contains
 				if (playground(i-1,j+1)) counter = counter + 1
 				if (playground(i,j+1)) counter = counter + 1
 				if (playground(i+1,j+1)) counter = counter + 1
-				neighbors(i,j) = counter
+				neighbors(i,j) = counter ! schreibe Anzahl an Nachbarn raus
 			enddo
 		enddo
 	end subroutine
 
 	subroutine developLife(playground)
-		logical :: playground(0:31,0:21)
-		integer :: i,j
-		integer :: neighbors(30,20)
+		logical :: playground(0:31,0:21)	! Spielfeld
+		integer :: i,j	! Zaehlindices
+		integer :: neighbors(30,20)	! Feld mit Anzahl an Nachbarn
+		
+		! zahle Nachbarn jeder Zelle
 		call countNeighbors(playground,neighbors)
+		
+		! setze Status jeder Zelle abhaengig von ihrer Anzahl an Nachbarn
 		do i = 1,30
 			do j = 1,20
+				! eine tote zelle mit genau 3 Nachbarn wird zur lebenden Zelle
 				if (.NOT.(playground(i,j)) .AND. neighbors(i,j) == 3) then
 					playground(i,j) = .TRUE.
+				! eine lebende Zelle stirbt bei weniger als 2 oder mehr als 3 Nachbarn
 				else if (playground(i,j) .AND. (neighbors(i,j)<2 .OR. neighbors(i,j) > 3)) then
 					playground(i,j) = .FALSE.
 				endif
